ac39289c11f2bbb8474a2f0d5c6cbdadaff90513
Michael Martin <mlmartin13@gmail.com>
2026-04-26 16:29:13 -0600

    Hide mac garbage from vim explorer

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -3,17 +3,44 @@ return {
   opts = {
     picker = {
       sources = {
-        -- Show hidden files in the sidebar explorer
+        -- Show hidden files in the sidebar explorer, but exclude macOS noise
         explorer = {
           hidden = true,
+          exclude = {
+            ".DS_Store",
+            ".localized",
+            ".Spotlight-V100",
+            ".Trashes",
+            ".fseventsd",
+            ".TemporaryItems",
+            "__MACOSX",
+          },
         },
-        -- Show hidden files in <leader>ff / find files
+        -- Show hidden files in <leader>ff / find files, but exclude macOS noise
         files = {
           hidden = true,
+          exclude = {
+            ".DS_Store",
+            ".localized",
+            ".Spotlight-V100",
+            ".Trashes",
+            ".fseventsd",
+            ".TemporaryItems",
+            "__MACOSX",
+          },
         },
-        -- Show hidden files in grep (<leader>sg etc.)
+        -- Show hidden files in grep (<leader>sg etc.), but exclude macOS noise
         grep = {
           hidden = true,
+          exclude = {
+            ".DS_Store",
+            ".localized",
+            ".Spotlight-V100",
+            ".Trashes",
+            ".fseventsd",
+            ".TemporaryItems",
+            "__MACOSX",
+          },
         },
       },
     },
