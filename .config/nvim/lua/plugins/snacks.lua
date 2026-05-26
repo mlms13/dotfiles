cc7d2050fce0a800821c9287309a56528e303ef5
Michael Martin <mlmartin13@gmail.com>
2026-05-26 12:53:25 -0600

    Show (and search) gitignored files in a project

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -6,6 +6,7 @@ return {
         -- Show hidden files in the sidebar explorer, but exclude macOS noise
         explorer = {
           hidden = true,
+          ignored = true,
           exclude = {
             ".DS_Store",
             ".localized",
@@ -19,6 +20,7 @@ return {
         -- Show hidden files in <leader>ff / find files, but exclude macOS noise
         files = {
           hidden = true,
+          ignored = true,
           exclude = {
             ".DS_Store",
             ".localized",
@@ -32,6 +34,7 @@ return {
         -- Show hidden files in grep (<leader>sg etc.), but exclude macOS noise
         grep = {
           hidden = true,
+          ignored = true,
           exclude = {
             ".DS_Store",
             ".localized",
