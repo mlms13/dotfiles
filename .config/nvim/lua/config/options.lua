f954376a2d636e27b2e11d8334984e57ec213848
Michael Martin <mlmartin13@gmail.com>
2026-05-31 13:49:36 -0600

    Better tmux styling and titles

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,3 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 
 vim.opt.colorcolumn = "80,120"
+
+-- Emit a terminal title so tmux's pane border shows the file, not the hostname
+vim.opt.title = true
+vim.opt.titlestring = "%t" -- just the filename; use "%f" for the relative path
