defd165816d5c8e3f7813c94c6cd9435b677a926
Michael Martin <mlmartin13@gmail.com>
2026-07-11 19:05:17 -0600

    Use project ts server in vim

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -4,6 +4,14 @@ return {
     opts = {
       servers = {
         pyright = {},
+        vtsls = {
+          settings = {
+            -- LazyVim defaults this to true, which makes vtsls drive the
+            -- workspace's TypeScript. vtsls 0.3.0 ships a 5.9-era host that
+            -- can't drive tsserver 6.x
+            vtsls = { autoUseWorkspaceTsdk = false },
+          },
+        },
       },
     },
   },
